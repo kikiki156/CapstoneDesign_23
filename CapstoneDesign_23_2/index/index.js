e41ddd8838253ea0a05030c7ea57d601e77849dd
kikiki156 <5555565@naver.com>
2023-11-10 01:23:04 +0900

    modify daily_checklist

--- CapstoneDesign_23_2/index/index.js
+++ CapstoneDesign_23_2/index/index.js
@@ -1,7 +1,13 @@
 function addChecklistItem() {
-    var cklist = document.querySelector('.row');
+    var cklist = document.getElementById('checklist-container');
     var newItem = document.createElement('div');
     newItem.classList.add('cklist');
-    newItem.innerHTML = '<input type="checkbox"> <input type="text" name="st_name">';
-    cklist.appendChild(newItem);
+    newItem.innerHTML = '<input type="checkbox"> <input type="text" name="st_name"> <span class="delete-btn" onclick="deleteChecklistItem(this)">삭제하기</span>';
+    cklist.insertBefore(newItem, cklist.lastChild);
+  }
+  
+  function deleteChecklistItem(element) {
+    var cklist = document.getElementById('checklist-container');
+    var itemToDelete = element.parentNode;
+    cklist.removeChild(itemToDelete);
   }
