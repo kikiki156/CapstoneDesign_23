aa920bba78c76f3ed078677945a55c72e8056332
hanseongmin <5555565@naver.com>
2023-12-01 01:29:52 +0900

    feat: add dbconn query logging

--- node/controller/dbconn.js
+++ node/controller/dbconn.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
 dotenv.config({ path: './config/.env' });
 const { Client } = require('pg');
-console.log(process.env.DB_USER);
+console.log(process.env.DB_USER, process.env.DB_HOST, process.env.DB_NAME, process.env.DB_PASSWD, process.env.DB_PORT);
 
 const pgConnection = new Client({
     user: process.env.DB_USER,
