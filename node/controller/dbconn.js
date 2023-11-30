137b79e939cffb6bc956e613f170cebe28a34a5b
hanseongmin <5555565@naver.com>
2023-12-01 01:17:32 +0900

    feat: using pool instead of client to connect to db

--- node/controller/dbconn.js
+++ node/controller/dbconn.js
@@ -1,10 +1,10 @@
-const {Client} = require('pg');
+const pg = require('pg');
 const dotenv = require('dotenv');
 dotenv.config({path: './config/.env'});
 
 console.log(process.env.DB_USER);
 
-const pgConnection = new Client({
+const pgConnection = new pg.Pool({
     user: process.env.DB_USER, host: process.env.DB_HOST, database: process.env.DB_NAME, password: process.env.DB_PASSWD, port: process.env.DB_PORT,
 })
 
