39a5ce6d001d7a1261a557b198ea700888ab4481
hanseongmin <5555565@naver.com>
2023-12-01 01:14:00 +0900

    feat: log dbconn using dotenv

--- node/controller/dbconn.js
+++ node/controller/dbconn.js
@@ -1,4 +1,8 @@
 const {Client} = require('pg');
+const dotenv = require('dotenv');
+dotenv.config({path: './config/.env'});
+
+console.log(process.env.DB_USER);
 
 const pgConnection = new Client({
     user: process.env.DB_USER, host: process.env.DB_HOST, database: process.env.DB_NAME, password: process.env.DB_PASSWD, port: process.env.DB_PORT,
