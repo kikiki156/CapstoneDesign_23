2a30e9e2d06f732e6500362dae2a6e498281902b
hanseongmin <5555565@naver.com>
2023-12-01 15:11:04 +0900

    fix loginpagelog

--- node/routes/settingpage/loginpage.js
+++ node/routes/settingpage/loginpage.js
@@ -7,7 +7,7 @@ router.get("/loginpage", (req, res) => {
     console.log("loginpage1234");
     res.render("settingpage/loginpage.html");
 });
-console.log(req);
+
 router.post("/loginpage/acceptLogin", controller.acceptLogin);
 router.post("/loginpage/acceptCreate", controller.acceptCreate);
 
