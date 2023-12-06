7c19d369c4383cf99203fdd22f9dc44ba30c8756
dusdnr1015 <81170163+dusdnr1015@users.noreply.github.com>
2023-12-06 22:21:54 +0900

    Update profilepage.js

--- node/routes/settingpage/profilepage.js
+++ node/routes/settingpage/profilepage.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-// const controller = require("../../controller/settingpage/profilepage");
-
 router.get("/profilepage", (req, res) => {
     console.log("profilepageeeeeeee");
     res.render("settingpage/profilepage.html");
