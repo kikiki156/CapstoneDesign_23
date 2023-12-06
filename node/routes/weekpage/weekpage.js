f4ebe5bfb18d61c926ab20edbce9e345a30a99f3
dusdnr1015 <81170163+dusdnr1015@users.noreply.github.com>
2023-12-06 22:21:36 +0900

    Update weekpage.js

--- node/routes/weekpage/weekpage.js
+++ node/routes/weekpage/weekpage.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-// const controller = require("../../controller/weekpage/weekpage");
-
 router.get("/weekpage", (req, res) => {
     console.log("weekpage");
     res.render("weekpage/weekpage.html");
